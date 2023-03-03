a49467ae01cdcbb9210279cf33abe9e879ed788a
Icey Python <samkellyndungu@gmail.com>
2023-03-03 18:08:27 +0300

    Added: logic to calculate the total price

--- book.php
+++ book.php
@@ -1,16 +1,19 @@
 <?php
 include 'header.php';
-require "config.php";
+include "config.php";
 if(isset($_GET['id'])){
-$id =$_GET['id'];
-$sql= "SELECT * FROM cars WHERE id= $id";
-$result= mysqli_query($con,$sql);
-$row = mysqli_fetch_array($result);
-$carName = $row['CarName'];
-$carPrice = $row['carPrice'];
-$NoDays= '?';
-$total= $carPrice*$NoDays;
-$cimage= $row['carImage'];
+  $id =$_GET['id'];
+  $sql= "SELECT * FROM cars WHERE id= $id";
+  $result= mysqli_query($con,$sql);
+  $row = mysqli_fetch_assoc($result);
+  $carName = $row['CarName'];
+  //var_dump($row);
+  //echo($row['carName']);
+  $carPrice = $row['carPrice'];
+  $NoDays= "<script> number</script>";
+  echo($NoDays);
+  //$total= $NoDays * intval(substr($carPrice,4,strlen($carPrice)-1));
+  $cimage= $row['carImage'];
 }
 else{
   echo'the car is not available';
@@ -33,29 +36,28 @@ else{
   </head>
   <body>
     <div clas="container">
-      <div class="row justify-content-center">
+      <div class="row justify-content-center w-100">
         <div class="col-md-8 my-5">
           <h2 class="text-center p-2 text-primary">Fill the following details to complete your booking</h2>
+          <img src="<?php echo $cimage?>" class="w-100 p-0 pb-3 rounded-1" >
         <h3>Car Details:</h3>
         <table class="table table-bordered p-3" width= "500px">
           <tr>
             <th>Car Name:</th>
-              <td><?php echo $carName ?></td>
+              <td><?php echo $row['carName']?></td>
           </tr>
           <tr>
             <th>Charges per Day:</th>
               <td><?php echo $carPrice ?></td>
           </tr>
-          <tr>
-            <th>Number of days:</th>
-              <td><?php echo $NoDays ?></td>
-          </tr>
-          <tr>
-            <th>Total Amount:</th>
-              <td><?php echo $total ?></td>
-          </tr>
-          
         </table>
+        <form>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Number of days</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Number of days" required>
+            </div>
+            <button type="submit" name='calculate_total' class="btn btn-primary">Calculate total</button>
+          </form>
         </div>
       </div>
     </div>
