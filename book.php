e918084aa1466b57cd2dd12f93d27b1bc18447be
Icey Python <samkellyndungu@gmail.com>
2023-03-04 17:00:08 +0300

    ADDED: logic to perfom MPESA STK push

--- book.php
+++ book.php
@@ -2,14 +2,18 @@
 include 'header.php';
 include "config.php";
 if(isset($_GET['id'])){
+  session_start();
   $id =$_GET['id'];
+  $_SESSION['id'] = $id;
   $sql= "SELECT * FROM cars WHERE id= $id";
   $result= mysqli_query($con,$sql);
   $row = mysqli_fetch_assoc($result);
   $carName = $row['CarName'];
+  $_SESSION['car'] = $carName;
   //var_dump($row);
   //echo($row['carName']);
   $carPrice = $row['carPrice'];
+  $_SESSION['price'] = $carPrice;
   $NoDays= 0;
   //$total= $NoDays * intval(substr($carPrice,4,strlen($carPrice)-1));
   $cimage= $row['carImage'];
@@ -38,7 +42,7 @@ else{
       <div class="row justify-content-center w-100">
         <div class="col-md-8 my-5">
           <h2 class="text-center p-2 text-primary">Fill the following details to complete your booking</h2>
-          <img src="<?php echo $cimage?>" class="w-50 p-0 pb-3 rounded-1 mx-auto" >
+          <img src="<?php echo $cimage?>" class="w-50 h-50 p-0 pb-3 rounded-3 mx-auto object-fit-cover" >
         <h3>Car Details:</h3>
         <table class="table table-bordered p-3" width= "500px">
           <tr>
@@ -51,10 +55,10 @@ else{
           </tr>
         </table>
         <div class="row">
-          <form class="col" action="setTotal.php" method="POST">
+          <form class="col" action="setTotal.php" method="POST" id="form">
               <div class="form-group">
                 <label for="exampleInputEmail1">Number of days</label>
-                <input type="text" pattern="[0-9]{2}" class="form-control w-auto" id="days"  placeholder="Enter Number of days" max=30 required>
+                <input type="text" pattern="[0-9]{1-2}" class="form-control w-auto" id="days"  placeholder="Enter Number of days" max=30 name="days" required>
               </div>
               <button type="submit" name='calculate_total' class="btn btn-primary">Calculate total</button>
             </form>
@@ -68,8 +72,12 @@ else{
         let c_price=`<?php echo $carPrice?>`;
         let multiplier = document.getElementById('days');
         let price_total = document.getElementById('price');
-        if((c_price.match(/ksh/gi)).length > 0){
-          c_price = c_price.replace('ksh. ','')
+        let form = document.getElementById("form");
+        form.onsubmit=(e)=>{
+          if(multiplier.value > 30){
+            e.preventDefault();
+            alert('Invalid number of days passed'); 
+          }
         }
         multiplier.oninput=()=>{
           if(Number(multiplier.value)<=30){
--- dashboard.php
+++ dashboard.php
@@ -1,30 +1,3 @@
 <?php 
     
 ?>
-
-   <div class="container">
-    <div class="row col-lg-4 mt-3 mb-3">
-  
-  <div class="card-deck" >
-   <div class="card boarder-info p-2" style= "height:500px; width:500px;"> 
-<img class="card-img-top" src= "images/'.$carImage.'"  style="height: 3   00px;">
-
-<br>
-<br>
-<br>
-<div class=" card-body">
-    <h4 class="card-tittle"> Name: ' . $name . '</h4>
-    <br>
-    <h4 class="card-tittle"> Price:  ' . $price . ' / day</h4>
-<a href="order.php? orderid=' . $id . ' "  class= "btn btn-danger btn-block btn-lg p-2" > Book Now </a>
-</div>
-  </div>
-  </div>
- </div>
- </div>
- ';
-  
- } 
-  }else mysqli_error($con);
-?>
-*/
