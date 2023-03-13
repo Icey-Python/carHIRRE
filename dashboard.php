a0009d48adf72bafe5cdcfba3bdd12c8472c7fec
regina-77 <kanjaregina824@gmail.com>
2023-03-13 09:13:18 +0300

    i tried creating a dashboard

--- dashboard.php
+++ dashboard.php
@@ -1,4 +1,41 @@
 <?php
 include'config.php';
-    include'header.php';
+    //include'header.php';
 ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>car rental management system</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  <link rel="stylesheet" href="dashboard.css">
+  </head>
+  <body>
+    <header class="header">
+        <a href="#">Admin dashboard</a>
+<div class="logout">
+<a href="logout.php" class="btn btn-primary">logout</a>
+</div>
+    </header>
+ <aside>
+    <ul>
+        <li>
+            <a href="#">addcar</a>
+        </li>
+        <li>
+            <a  href="#">viewcars</a>
+        </li>
+        <li>
+            <a  href="#">viewbookings</a>
+        </li>
+       
+    </ul>
+ </aside>
+
+
+    
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+  </body>
+</html>
