74c6ae71f6edd24fe519ab71823d3a054ceea69f
Icey Python <samkellyndungu@gmail.com>
2023-04-04 17:25:07 +0300

    Added: Feedback system

--- landingpage.php
+++ landingpage.php
@@ -11,10 +11,12 @@ if(!isset($_SESSION["name"])){
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>car rental management system</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://static.elfsight.com/platform/platform.js" data-use-service-core defer></script>
   <link rel="stylesheet" href="landingpage.css">
   </head>
   <body>
-    
+  
+
   <nav class="navbar navbar-expand-lg bg-dark">
   <div class="container-fluid">
     <a class="navbar-brand text-primary">Regie motors</a>
@@ -42,13 +44,14 @@ if(!isset($_SESSION["name"])){
   </div>
 </nav>
 
-<div>
+
 <h1 class="landing">WELCOME TO REGIE MOTORS</h1>
     <p class="text-center"> we offer quality services. Make a reservation with us.
       <br>
       Thank you.
     </p>
-</div>
+
+  <div class="elfsight-app-e71c0e19-63ff-48e8-9e01-c4a71f612439"></div>
     
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
